f6a70a0d54d2da20cf5eb0a9715fa58347d7f98d
Kirill Rudenko <cs1@list.ru>
2022-10-22 17:21:34 +0300

    fix Pokemon.image model field (allow null)

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,7 +3,7 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='media', blank=True)
+    photo = models.ImageField(upload_to='media', null=True)
 
     def __str__(self) -> str:
         return f'{self.title}'
